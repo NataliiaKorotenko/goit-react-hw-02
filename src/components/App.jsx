05593832f31dc1a0df0cfb37e43d84ac89bc5362
NataliiaKorotenko <119405955+NataliiaKorotenko@users.noreply.github.com>
2024-08-13 21:12:40 +0200

    description

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import '../components/App.css'
+import Description from './Description/Description'
+
+function App() {
+ 
+  return (
+     <Description/>
+  )
+}
+
+export default App;
